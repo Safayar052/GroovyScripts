8949f7172f34fab26883e60d9443e1a74ea518b7
sahmed052 <safayar@sybrandsplace.nl>
2024-04-15 09:04:13 +0200

    parse only log files with WA prefix

--- webadapter_logparse/logparse.groovy
+++ webadapter_logparse/logparse.groovy
@@ -1,12 +1,17 @@
 // Import necessary classes
 import java.nio.file.*
 import java.nio.charset.*
+import java.util.Properties
+
+// Load the properties file
+def properties = new Properties()
+properties.load(new File('config.properties').newReader())
 
 // ISH pipelien search token
-def searchTokenPipeline = "ViewParametricSearch"
+def searchTokenPipeline = properties.getProperty('searchTokenPipeline')
 
 // REST call search token
-def searchTokenRest = "products?searchTerm"
+def searchTokenRest = properties.getProperty('searchTokenRest')
 
 
 // Regex pattern to get search term
@@ -19,27 +24,33 @@ if (args.length > 0) {
     // Get directory path
     def directoryPath = args[0]
 
-    // Traverse each file in the directory
-    Files.walk(Paths.get(directoryPath)).each { path ->
-        if (Files.isDirectory(path)) {
-            // Skip directories
-            return
-        }
+    
+    //sample code for read file with log extenstion
 
-        // Read each line of the file and process it
-        Files.lines(path, StandardCharsets.UTF_8).each { line ->
+    // Define the directory path
+    def directory = new File(directoryPath)
+
+    // Filter files with .log extension and a certain prefix
+    def logFiles = directory.listFiles { file ->
+        file.isFile() && file.name.startsWith("WA-") && file.name.endsWith(".log")
+    }
+
+    // Process the filtered files
+    logFiles.each { file ->
+        file.eachLine { line ->
             if (line.contains(searchTokenPipeline) || line.contains(searchTokenRest)) {
                 def matcher = (line =~ pattern)
 
                 if (matcher.find()) {
                     def searchTerm = matcher.group(1)
-                    if (searchTerm.length() > 2) {
-                        // Find the index of the first space character
-                        def indexOfSpace = searchTerm.indexOf(' ')
+                
+                    // Find the index of the first space character
+                    def indexOfSpace = searchTerm.indexOf(' ')
 
-                        // If a space is found, trim all characters after it
-                        def formattedSearchTerm = (indexOfSpace >= 0 ? searchTerm.substring(0, indexOfSpace) : searchTerm).replaceAll("\\+", " ")
-
+                    // If a space is found, trim all characters after it
+                    def formattedSearchTerm = ((indexOfSpace >= 0 ? searchTerm.substring(0, indexOfSpace) : searchTerm).replaceAll("\\+", " ")).trim()
+                    
+                    if (formattedSearchTerm.length() > 2) {
                         uniqueSearchKeys.add(formattedSearchTerm)
                     }
                 }
@@ -50,6 +61,7 @@ if (args.length > 0) {
     println uniqueSearchKeys
 
     println uniqueSearchKeys.size()
+
 } else {
     println "No directory path is provided"
 }
